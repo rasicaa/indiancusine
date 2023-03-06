3642a2ec731a2ce0bd040c4c9bfbf409c8c87169
rasicaa <ghormoderasika220697@gmail.com>
2023-03-06 16:16:17 +0530

    hello world

---
+++ src/main/java/com/example/mainmenums/MainmenuMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainmenums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainmenuMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainmenuMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mainmenums/MainmenuMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainmenums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainmenuMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
